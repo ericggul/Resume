e01118d4eac3d904e842a9a8303d16aad6857758
ericggul <57379903+ericggul@users.noreply.github.com>
2021-02-12 22:52:04 +0900

    Updating.. Button Functionality on progress

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const toTop = document.querySelector(".to-top")
 
+
 window.addEventListener("scroll", () => {
     if (window.pageYOffset >100){
         toTop.classList.add("active");
@@ -7,3 +8,15 @@ window.addEventListener("scroll", () => {
         toTop.classList.remove("active");
     }
 })
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function(){
+    var currentScrollpos = window.pageYOffset;
+    if (prevScrollpos>currentScrollpos){
+        document.getElementsByClassName(".resume_skills").style.top = "0";
+    } else {
+        document.getElementsByClassName(".resume_skills").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollpos;
+}
