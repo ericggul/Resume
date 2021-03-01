6c5c951beedf26574f56d5d253723174a5485985
ericggul <57379903+ericggul@users.noreply.github.com>
2021-03-01 16:55:38 +0900

    Fixed

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const toTop = document.querySelector(".to-top")
-
+const resumeSkills = document.querySelector(".resume_skills")
 
 window.addEventListener("scroll", () => {
     if (window.pageYOffset >100){
         toTop.classList.add("active");
     } else{
         toTop.classList.remove("active");
+    }
+})
+
+window.addEventListener("scroll", () =>{
+    if (window.pageYOffset > resumeSkills.offsetTop){
+        resumeSkills.classList.add("sticky");
+    } else {
+        resumeSkills.classList.remove("sticky");
     }
 })
 
